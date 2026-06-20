eb79d71e42cbd0c4b0816879942dcf978049f7f1
foolvan <ivanmedvedev1357@gmail.com>
2026-06-20 15:24:48 +0300

    qwerty

--- infra/gateway-server/src/main/java/ru/practicum/gateway/GatewayServerApplication.java
+++ infra/gateway-server/src/main/java/ru/practicum/gateway/GatewayServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServerApplication {
+    //арпомр
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
